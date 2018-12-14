52b341cc3283c1547e38e0e35b8bbc81814bc600
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-14 14:37:13 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
 console.log(req.body);
 })
 
+app.get('/todos', (req,res) => {
+  Todo.find().then(todos => {
+    res.send({todos})
+  }, err => {
+    res.status(400).send(err);
+  })
+})
+
 app.listen(3000, () => {
   console.log('server is running on port 3000')
 })
