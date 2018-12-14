a12b803d59524e04c85e970f336d0a3d235e4462
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-14 15:20:59 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -27,6 +28,18 @@ app.get('/todos', (req,res) => {
   }, err => {
     res.status(400).send(err);
   })
+})
+
+app.get('/todos/:id', (req,  res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send('id is not valid');
+  } else {
+    Todo.findById(id).then(todo => {
+      if (todo) res.send({todo})
+      else res.status(404).send('there is no todo with this id')
+    }, err => res.status(400).send(err))
+  }
 })
 
 app.listen(3000, () => {
