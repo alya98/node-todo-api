b0fcf18325195468a5221267f6caadd0c1db8571
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-17 18:10:21 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,26 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 4,
-};
+const password = 'qwer1234!';
 
-const token = jwt.sign(data, '123qwer');
-console.log(token);
-const decoded = jwt.verify(token, '123qwer')
-console.log('decoded: ', decoded)
+bcrypt.genSalt(10, (err,salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash)
+  })
+});
+
+const hashedPassword = '$2a$10$557x9c6M1szLCYLPTRrhLOXWoAz.y2NlJruYIHLQcShOV3DmNb52.';
+
+bcrypt.compare('qwer1234', hashedPassword, (err, res) => console.log(res))
+// const data = {
+//   id: 4,
+// };
+
+// const token = jwt.sign(data, '123qwer');
+// console.log(token);
+// const decoded = jwt.verify(token, '123qwer')
+// console.log('decoded: ', decoded)
 
 // const message = 'i am alina';
 
